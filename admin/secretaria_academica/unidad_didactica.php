f74bb0a4691895e0178bb732ac0c69152fa4842b
anibalyucra20 <anibalyucrac@gmail.com>
2022-10-10 22:01:55 -0500

    Update unidad_didactica.php

--- admin/secretaria_academica/unidad_didactica.php
+++ admin/secretaria_academica/unidad_didactica.php
@@ -9,7 +9,7 @@ include '../include/busquedas.php';
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="Content-Language" content="es-ES">
     <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	  
