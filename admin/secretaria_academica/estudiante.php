46b1f57da423388c6cf0d20e3699935933b8c6cc
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-01 11:19:12 -0500

    Update estudiante.php

--- admin/secretaria_academica/estudiante.php
+++ admin/secretaria_academica/estudiante.php
@@ -252,7 +252,6 @@ include '../include/busquedas.php';
                           <br>
                         </div>
                       </div>
-                      </div>
                       <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Sección : </label>
                         <div class="col-md-9 col-sm-9 col-xs-12">
