d421b69da3d8f1784e61b3b2ed4a982ebe90065b
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-01 19:32:12 -0500

    Update actualizar_estudiante.php

--- admin/secretaria_academica/operaciones/actualizar_estudiante.php
+++ admin/secretaria_academica/operaciones/actualizar_estudiante.php
@@ -21,7 +21,7 @@ $discapacidad = $_POST['discapacidad'];
 $busc_est_car = "SELECT * FROM estudiante WHERE dni='$dni' AND id_programa_estudios='$carrera'";
 $ejec_busc_est_car = mysqli_query($conexion, $busc_est_car);
 $conteo = mysqli_num_rows($ejec_busc_est_car);
-if((($dni_a != $dni) && ($conteo == 0)) || (($dni_a == $dni)&&($conteo == 1))){
+if((($dni_a <> $dni) && ($conteo == 0)) || (($dni_a == $dni)&&($conteo == 1))){
     $sql = "UPDATE estudiante SET dni='$dni', apellidos_nombres='$nom_ap', id_genero='$genero', fecha_nac='$fecha_nac', direccion='$direccion', correo='$email', telefono='$telefono', anio_ingreso='$anio_ingreso', id_programa_estudios='$carrera', id_semestre='$semestre', seccion='$seccion', turno='$turno', id_condicion='$condicion', discapacidad='$discapacidad' WHERE id=$id";
     $ejec_consulta = mysqli_query($conexion, $sql);
     if ($ejec_consulta) {
