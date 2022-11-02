5802bf9d4ceafb3f3f5c700e32ad1f7855852ee1
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-01 21:18:43 -0500

    Update actualizar_estudiante.php

--- admin/secretaria_academica/operaciones/actualizar_estudiante.php
+++ admin/secretaria_academica/operaciones/actualizar_estudiante.php
@@ -21,7 +21,24 @@ $discapacidad = $_POST['discapacidad'];
 $busc_est_car = "SELECT * FROM estudiante WHERE dni='$dni' AND id_programa_estudios='$carrera'";
 $ejec_busc_est_car = mysqli_query($conexion, $busc_est_car);
 $conteo = mysqli_num_rows($ejec_busc_est_car);
-if((($dni_a <> $dni) && ($conteo == 0)) || (($dni_a == $dni)&&($conteo == 0))){
+if(($dni_a <> $dni) && ($conteo == 0)){
+    $sql = "UPDATE estudiante SET dni='$dni', apellidos_nombres='$nom_ap', id_genero='$genero', fecha_nac='$fecha_nac', direccion='$direccion', correo='$email', telefono='$telefono', anio_ingreso='$anio_ingreso', id_programa_estudios='$carrera', id_semestre='$semestre', seccion='$seccion', turno='$turno', id_condicion='$condicion', discapacidad='$discapacidad' WHERE id=$id";
+    $ejec_consulta = mysqli_query($conexion, $sql);
+    if ($ejec_consulta) {
+        echo "<script>
+			alert('Registro Actualizado de manera Correcta');
+			window.location= '../estudiante.php';
+		</script>
+	";
+    }else {
+        echo "<script>
+			alert('Error al Actualizar Registro, por favor contacte con el administrador');
+			window.history.back();
+		</script>
+	";
+    }
+
+}elseif (($dni_a == $dni)) {
     $sql = "UPDATE estudiante SET dni='$dni', apellidos_nombres='$nom_ap', id_genero='$genero', fecha_nac='$fecha_nac', direccion='$direccion', correo='$email', telefono='$telefono', anio_ingreso='$anio_ingreso', id_programa_estudios='$carrera', id_semestre='$semestre', seccion='$seccion', turno='$turno', id_condicion='$condicion', discapacidad='$discapacidad' WHERE id=$id";
     $ejec_consulta = mysqli_query($conexion, $sql);
     if ($ejec_consulta) {
@@ -40,11 +57,10 @@ if((($dni_a <> $dni) && ($conteo == 0)) || (($dni_a == $dni)&&($conteo == 0))){
 
 }else{
     echo "<script>
-			alert('Error, no se puede registrar el mismo estudiante mas de 1 vez');
-			window.history.back();
-		</script>
-	";
-
+    alert('Error, no se puede registrar el mismo estudiante mas de 1 vez');
+    window.history.back();
+</script>
+";
 }
 
 mysqli_close($conexion);
