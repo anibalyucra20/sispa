9676c9834e0214dbd27a4b3887d6bf60edc8d558
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-13 04:10:30 -0500

    Update acciones_programa_estudio.php

--- admin/secretaria_academica/include/acciones_programa_estudio.php
+++ admin/secretaria_academica/include/acciones_programa_estudio.php
@@ -58,7 +58,7 @@
                       <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Perfil de Egresado : </label>
                         <div class="col-md-9 col-sm-9 col-xs-12">
-                        <textarea class="form-control"  rows="3" style="width: 100%; min-height: 165px; height: auto" name="perfil_egreso" required="required"><?php echo $res_busc_carrera['perfil_egresado']; ?></textarea>
+                        <textarea class="form-control"  rows="3" style="width: 100%; min-height: 165px; height: auto;" name="perfil_egreso" required="required"><?php echo $res_busc_carrera['perfil_egresado']; ?></textarea>
                           <br>
                           <br>
                         </div>
