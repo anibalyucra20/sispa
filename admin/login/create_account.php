d37084e5f3c6c00d9101ce90447e116c0662172b
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-13 09:02:42 -0500

    Update create_account.php

--- admin/login/create_account.php
+++ admin/login/create_account.php
@@ -5,16 +5,13 @@ include "../include/busquedas.php";
 $id = $_POST['data'];
 $password = $_POST['password'];
 $dni = $_POST['dni'];
-// validar si el docente ya tiene usuario
-$busc_usu_doc = buscarUsuarioDocenteById($conexion, $id);
-$cont_busc_usu_doc = mysqli_num_rows($busc_usu_doc);
 
-if ($cont_busc_usu_doc == 0) {
-	$busc_doc = buscarDocenteByDni($conexion, $dni);
+	$busc_doc = buscarDocenteById($conexion, $id);
 	$cont_b_doc = mysqli_num_rows($busc_doc);
-	if ($cont_b_doc > 0) {
+	$res_b_docc = mysqli_fetch_array($busc_doc);
+	if ($res_b_docc['dni']==$dni) {
 		$pass_secure = password_hash($password, PASSWORD_DEFAULT);
-		$crear_user = "INSERT INTO usuarios_docentes (id_docente, usuario, password) VALUES ('$id', '$dni', '$pass_secure')";
+		$crear_user = "UPDATE docente SET password='$pass_secure' WHERE id=$id";
 		$ejec_crear_user = mysqli_query($conexion, $crear_user);
 		if ($ejec_crear_user) {
 			echo "<script>
@@ -35,12 +32,6 @@ if ($cont_busc_usu_doc == 0) {
 				</script>
 			";
 	}
-}else{
-	echo "<script>
-                alert('Ud. Ya cuenta con una cuenta, por favor contacte con el administrador');
-                window.location= '../login/'
-    </script>";
-}
 
 
 ?>
