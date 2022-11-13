d0b3269e40c95b0b9c52d7f10e3fc8da24107c3e
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-13 18:39:12 -0500

    Update registrar_indicador_logro_capacidad.php

--- admin/secretaria_academica/operaciones/registrar_indicador_logro_capacidad.php
+++ admin/secretaria_academica/operaciones/registrar_indicador_logro_capacidad.php
@@ -8,7 +8,8 @@ $descripcion = $_POST['descripcion'];
 //buscar codigo para asignar
     $buscar_indicador_capacidad = buscarIndicadorLogroCapacidadByIdCapacidad($conexion, $id_capacidad);
     $conteo_res = mysqli_num_rows($buscar_indicador_capacidad);
-    $codigo = "I".$conteo_res+1;
+    $conteo = $conteo_res+1;
+    $codigo = "I".$conteo;
 
 	$insertar = "INSERT INTO indicador_logro_capacidad (id_capacidad, codigo, descripcion) VALUES ('$id_capacidad', '$codigo', '$descripcion')";
 	$ejecutar_insetar = mysqli_query($conexion, $insertar);
