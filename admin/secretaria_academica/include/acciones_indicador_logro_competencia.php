be007cb1be6e1788f5ef413648ec089408b7966c
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-13 18:01:48 -0500

    Update acciones_indicador_logro_competencia.php

--- admin/secretaria_academica/include/acciones_indicador_logro_competencia.php
+++ admin/secretaria_academica/include/acciones_indicador_logro_competencia.php
@@ -22,7 +22,7 @@
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Descripción : </label>
                         <div class="col-md-9 col-sm-9 col-xs-12">
                             <input type="number" name="correlativo" value="<?php echo $res_busc_indicadores['correlativo']; ?>" required="required">
-                          <br>
+                          <br><br>
                         </div>
                       </div>
                       <div class="form-group">
