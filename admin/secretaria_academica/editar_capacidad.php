4e6c8a7ad8eca6af7a67dda25b5d30ca3876ee8a
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-13 14:54:00 -0500

    edit capacidad

--- admin/secretaria_academica/editar_capacidad.php
+++ admin/secretaria_academica/editar_capacidad.php
@@ -3,6 +3,13 @@ include 'include/verificar_sesion.php';
 include '../../include/conexion.php';
 include '../include/busquedas.php';
 
+$id_competencia = $_GET['id'];
+$ejec_busc_comp = buscarCompetenciasById($conexion, $id_competencia); 
+$res_busc_comp=mysqli_fetch_array($ejec_busc_comp);
+$id_modulo = $res_busc_comp['id_modulo_formativo'];
+$busc_modulo = buscarModuloFormativoById($conexion, $id_modulo);
+$res_b_modulo = mysqli_fetch_array($busc_modulo);
+$id_p_e = $res_b_modulo['id_programa_estudio'];
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -10,13 +17,13 @@ include '../include/busquedas.php';
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="Content-Language" content="es-ES">
     <!-- Meta, title, CSS, favicons, etc. -->
-    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	  
-    <title>Capacidades<?php include ("../../include/header_title.php"); ?></title>
+    <title>Competencias <?php include ("../../include/header_title.php"); ?></title>
     <!--icono en el titulo-->
-    <link rel="shortcut icon" href="../../img/favicon.ico">
+    <link rel="shortcut icon" href="../img/favicon.ico">
     <!-- Bootstrap -->
     <link href="../../Gentella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
@@ -25,13 +32,18 @@ include '../include/busquedas.php';
     <link href="../../Gentella/vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
     <link href="../../Gentella/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-    <!-- Datatables -->
-    <link href="../../Gentella/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
-    <link href="../../Gentella/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
-    <link href="../../Gentella/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
-    <link href="../../Gentella/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
-    <link href="../../Gentella/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
-
+    <!-- bootstrap-wysiwyg -->
+    <link href="../../Gentella/vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
+    <!-- Select2 -->
+    <link href="../../Gentella/vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+    <!-- Switchery -->
+    <link href="../../Gentella/vendors/switchery/dist/switchery.min.css" rel="stylesheet">
+    <!-- starrr -->
+    <link href="../../Gentella/vendors/starrr/dist/starrr.css" rel="stylesheet">
+    <!-- bootstrap-daterangepicker -->
+    <link href="../../Gentella/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    
+    
     <!-- Custom Theme Style -->
     <link href="../../Gentella/build/css/custom.min.css" rel="stylesheet">
     <!-- Script obtenido desde CDN jquery -->
@@ -58,83 +70,23 @@ include '../include/busquedas.php';
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="">
-                    <h2 align="center">Capacidades</h2>
-                    <button class="btn btn-success" data-toggle="modal" data-target=".registrar"><i class="fa fa-plus-square"></i> Nuevo</button>
+                    <h2 align="center">Editar Capacidad</h2>
+                   
 
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
                     <br />
 
-                    <table id="example" class="table table-striped table-bordered" style="width:100%">
-                      <thead>
-                        <tr>
-                          <th>Identificador</th>
-                          <th>Programa de Estudios</th>
-                          <th>Módulo</th>
-                          <th>Competencia</th>
-                          <th>Codigo</th>
-                          <th>Descripcion</th>
-                          <th>Acciones</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <?php 
-                          $ejec_busc_capacidades = buscarCapacidades($conexion); 
-                          while ($res_busc_cap=mysqli_fetch_array($ejec_busc_capacidades)){
-                  
-                        ?>
-                        <tr>
-                          <td><?php echo $res_busc_cap['id']; ?></td>
-                          <?php 
-                          $id_competencia = $res_busc_cap['id_competencia'];
-                          $busc_competencia = buscarCompetenciasById($conexion, $id_competencia);
-                          $res_b_competencia = mysqli_fetch_array($busc_competencia);
-                          $id_modulo = $res_b_competencia['id_modulo_formativo'];
-                          $ejec_busc_modulo = buscarModuloFormativoById($conexion, $id_modulo);
-                          $res_busc_modulo =mysqli_fetch_array($ejec_busc_modulo);
-                          $id_carrera = $res_busc_modulo['id_programa_estudio'];
-                          $ejec_busc_carrera = buscarCarrerasById($conexion, $id_carrera);
-                          $res_busc_carrera =mysqli_fetch_array($ejec_busc_carrera);
-                          ?>
-                          <td><?php echo $res_busc_carrera['nombre']; ?></td>
-                          <?php 
-                          
-                          ?>
-                          <td><?php echo "M".$res_busc_modulo['nro_modulo']." - ".$res_busc_modulo['descripcion']; ?></td>
-                          <td><?php echo $res_b_competencia['codigo']; ?></td>
-                          <td><?php echo $res_busc_cap['codigo']; ?></td>
-                          <td><?php echo $res_busc_cap['descripcion']; ?></td>
-                          <td>
-                            <a class="btn btn-success" href="editar_capacidad.php?id=<?php echo $res_busc_cap['id']; ?>"><i class="fa fa-pencil-square-o"></i> Editar</a></td>
-                        </tr>  
-                        <?php
-                          };
-                        ?>
-
-                      </tbody>
-                    </table>
-                    <!--MODAL REGISTRAR-->
-                    <div class="modal fade registrar" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog modal-lg">
-                      <div class="modal-content">
-
-                        <div class="modal-header">
-                          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-                          </button>
-                          <h4 class="modal-title" id="myModalLabel" align="center">Registrar Capacidad</h4>
-                        </div>
-                        <div class="modal-body">
-                          <!--INICIO CONTENIDO DE MODAL-->
+                    
+                    
                   <div class="x_panel">
                     
-                  <div class="" align="center">
-                    <h2 ></h2>
-                    <div class="clearfix"></div>
-                  </div>
+                  
                   <div class="x_content">
                     <br />
-                    <form role="form" action="operaciones/registrar_capacidad.php" class="form-horizontal form-label-left input_mask" method="POST" >
+                    <form role="form" action="operaciones/actualizar_competencia.php" class="form-horizontal form-label-left input_mask" method="POST" enctype="multipart/form-data">
+                      <input type="hidden" name="id" value="<?php echo $id_competencia; ?>">
                       
                       <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Programa de Estudios : </label>
@@ -199,20 +151,17 @@ include '../include/busquedas.php';
                           <br>
                         </div>
                       </div>
-                      
                       <div align="center">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                          
+                        
+                          <a href="javascript:history.back(-1);" class="btn btn-primary">Cancelar</a>
                           <button type="submit" class="btn btn-primary">Guardar</button>
                       </div>
                     </form>
                   </div>
                 </div>
                           <!--FIN DE CONTENIDO DE MODAL-->
-                  </div>
-                </div>
-              </div>
-            </div>
+                 
+            
 
 
           </div>
@@ -229,35 +178,48 @@ include '../include/busquedas.php';
          /footer content -->
       </div>
     </div>
-   <!-- jQuery -->
-   <script src="../../Gentella/vendors/jquery/dist/jquery.min.js"></script>
+
+    <!-- jQuery -->
+    <script src="../../Gentella/vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
     <script src="../../Gentella/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- FastClick -->
     <script src="../../Gentella/vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
     <script src="../../Gentella/vendors/nprogress/nprogress.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="../../Gentella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
     <!-- iCheck -->
     <script src="../../Gentella/vendors/iCheck/icheck.min.js"></script>
-    <!-- Datatables -->
-    <script src="../../Gentella/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-    <script src="../../Gentella/vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
-    <script src="../../Gentella/vendors/jszip/dist/jszip.min.js"></script>
-    <script src="../../Gentella/vendors/pdfmake/build/pdfmake.min.js"></script>
-    <script src="../../Gentella/vendors/pdfmake/build/vfs_fonts.js"></script>
-
+    <!-- bootstrap-daterangepicker -->
+    <script src="../../Gentella/vendors/moment/min/moment.min.js"></script>
+    <script src="../../Gentella/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!-- bootstrap-wysiwyg -->
+    <script src="../../Gentella/vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js"></script>
+    <script src="../../Gentella/vendors/jquery.hotkeys/jquery.hotkeys.js"></script>
+    <script src="../../Gentella/vendors/google-code-prettify/src/prettify.js"></script>
+    <!-- jQuery Tags Input -->
+    <script src="../../Gentella/vendors/jquery.tagsinput/src/jquery.tagsinput.js"></script>
+    <!-- Switchery -->
+    <script src="../../Gentella/vendors/switchery/dist/switchery.min.js"></script>
+    <!-- Select2 -->
+    <script src="../../Gentella/vendors/select2/dist/js/select2.full.min.js"></script>
+    <!-- Parsley -->
+    <script src="../../Gentella/vendors/parsleyjs/dist/parsley.min.js"></script>
+    <!-- Autosize -->
+    <script src="../../Gentella/vendors/autosize/dist/autosize.min.js"></script>
+    <!-- jQuery autocomplete -->
+    <script src="../../Gentella/vendors/devbridge-autocomplete/dist/jquery.autocomplete.min.js"></script>
+    <!-- starrr -->
+    <script src="../../Gentella/vendors/starrr/dist/starrr.js"></script>
     <!-- Custom Theme Scripts -->
     <script src="../../Gentella/build/js/custom.min.js"></script>
+
+
+	  <!--prueba tabla-->
+    
+    <script src="../../include/tabla/jquery.dataTables.min.js"></script>
+    <script src="../../include/tabla/dataTables.bootstrap.min.js"></script>
     <script>
     $(document).ready(function() {
     $('#example').DataTable({
