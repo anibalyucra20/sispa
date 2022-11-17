57ee0c1a601fb00ca647e75027b4f4d92c803b43
anibalyucra20 <anibalyucrac@gmail.com>
2022-11-17 08:50:55 -0500

    Update registrar_programacion.php

--- admin/secretaria_academica/operaciones/registrar_programacion.php
+++ admin/secretaria_academica/operaciones/registrar_programacion.php
@@ -29,9 +29,13 @@ if($conteo_b_programacion_existe > 0){
     //crear silabo de la programacion hecha
     $reg_silabo = "INSERT INTO silabo (id_programacion_unidad_didactica, metodologia, recursos_didacticos, sistema_evaluacion, estrategia_evaluacion_indicadores, estrategia_evaluacion_tecnica, promedio_indicadores_logro, recursos_bibliograficos_impresos, recursos_bibliograficos_digitales) VALUES ('$id_programacion',' ',' ',' ',' ',' ',' ',' ',' ')";
     $ejec_reg_silabo = mysqli_query($conexion, $reg_silabo);
-    /*if (!$ejec_reg_silabo) {
-        echo " error silabo".$id_programacion." <br>";
-    }*/
+    if (!$ejec_reg_silabo) {
+        echo "<script>
+			alert('Error al Registrar Silabo');
+			window.history.back();
+		</script>
+		";
+    }
 
     //buscamos el id del silabo registrado mediante el id de la programacion
     $id_silabo = mysqli_insert_id($conexion);
@@ -52,18 +56,14 @@ if($conteo_b_programacion_existe > 0){
     for ($i=1; $i <=16 ; $i++) { 
         $reg_prog_act_silabo = "INSERT INTO programacion_actividades_silabo (id_silabo, semana, fecha, elemento_capacidad, actividades_aprendizaje, contenidos_basicos, tareas_previas) VALUES ('$id_silabo', '$i', '$hoy',' ',' ',' ',' ')";
         $ejec_reg_prog_act_silabo = mysqli_query($conexion, $reg_prog_act_silabo);
-        /*if (!$ejec_reg_prog_act_silabo) {
-            echo " error programacion".$id_silabo." <br>";
-        }*/
+        
         //buscamos el id de lo ingresado para registrar la siguiente tabla
         $id_prog_act_silabo = mysqli_insert_id($conexion);
         
         //procedemos a registrar la tabla sesion_aprendizaje-- 1 para cada tabla anterior
         $reg_sesion_aprendizaje = "INSERT INTO sesion_aprendizaje (id_programacion_actividad_silabo, tipo_actividad, tipo_sesion, fecha_desarrollo, id_ind_logro_competencia_vinculado, id_ind_logro_capacidad_vinculado, logro_sesion, bibliografia_obligatoria_docente, bibliografia_opcional_docente, bibliografia_obligatoria_estudiantes, bibliografia_opcional_estudiante, anexos) VALUES ('$id_prog_act_silabo',' ',' ','$hoy', '$id_ind_logro_competencia', '$id_ind_logro_capacidad',' ',' ',' ',' ',' ',' ')";
         $ejec_reg_sesion = mysqli_query($conexion, $reg_sesion_aprendizaje);
-        /*if (!$ejec_reg_sesion) {
-            echo " error sesion ".$id_prog_act_silabo." <br>";
-        }*/
+        
         //buscamos el id de la anterior tabla para hacer registros en las siguientes tablas
         $id_sesion = mysqli_insert_id($conexion);
 
@@ -75,17 +75,41 @@ if($conteo_b_programacion_existe > 0){
             // momentos de la sesion
             $reg_momentos_sesion = "INSERT INTO momentos_sesion_aprendizaje (id_sesion_aprendizaje, momento, estrategia, actividad, recursos, tiempo) VALUES ('$id_sesion', '$momento',' ',' ',' ',45)";
             $ejec_reg_momentos_sesion = mysqli_query($conexion, $reg_momentos_sesion);
-            /*if (!$ejec_reg_momentos_sesion) {
-                echo " error moomento1 <br>";
-            }*/
+            
             // actividad de evaluacion en la sesion
             $reg_act_eva = "INSERT INTO actividad_evaluacion_sesion_aprendizaje (id_sesion_aprendizaje, indicador_logro_sesion, tecnica, instrumentos, peso, momento) VALUES ('$id_sesion',' ',' ',' ',33,'$momento')";
             $ejec_reg_act_eva = mysqli_query($conexion, $reg_act_eva);
-            /*if (!$ejec_reg_act_eva) {
-                echo " error moomento2 <br>";
-            }*/
+            
         }
     }
+    if (!$ejec_reg_prog_act_silabo) {
+            echo "<script>
+			alert('Error al Generar las programacion para el silabo');
+			window.history.back();
+		</script>
+		";
+        }
+    if (!$ejec_reg_sesion) {
+            echo "<script>
+			alert('Error al Registrar las sesiones de aprendizaje');
+			window.history.back();
+		</script>
+		";
+        }
+    if (!$ejec_reg_momentos_sesion) {
+                echo "<script>
+			        alert('Error al Registrar Momentos de la sesion M1');
+			        window.history.back();
+		        </script>
+		        ";
+            }
+    if (!$ejec_reg_act_eva) {
+                echo "<script>
+			        alert('Error al Registrar Momentos de la sesion M2');
+			        window.history.back();
+		        </script>
+		        ";
+            }
     echo "<script>
                 alert('Registro Exitoso');
                 window.location= '../programacion.php'
