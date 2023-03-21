b1c87c736284ea21cc2b996648bbb0ed8d15c3f3
anibalyucra20 <anibalyucrac@gmail.com>
2023-03-21 01:53:20 -0500

    Update matricular_masiva.php

--- admin/matricular_masiva.php
+++ admin/matricular_masiva.php
@@ -3,8 +3,9 @@ $archivo = $_FILES['my_file_input'];
 $archivo = file_get_contents($archivo['tmp_name']);
 $archivo = explode("/n", $archivo);
 $archivo = array_filter($archivo);
-foreach ($estudiantes as $estudiante) {
-    $ests[] = explode(",", $estudiante);
+
+foreach ($estudiantes as $key => $value) {
+    $ests[] = explode(",", $key);
 }
 
 print_r($ests);
