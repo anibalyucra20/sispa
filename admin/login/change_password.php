b0267790b1d844d10dde07e653ca2dbbe535a4da
anibalyucra20 <anibalyucrac@gmail.com>
2023-03-21 00:43:58 -0500

    Update change_password.php

--- admin/login/change_password.php
+++ admin/login/change_password.php
@@ -5,7 +5,8 @@ $pass = $_POST['new_password'];
 $pass_secure = password_hash($pass, PASSWORD_DEFAULT);
 $b_doc = buscarDocenteById($conexion, $id);
 $r_b_doc = mysqli_fetch_array($b_doc);
-if ($r_b_doc['reset_password']==0) {
+$validar = $r_b_doc['reset_password'];
+if ($validar==0) {
 	//procedemos a actualizar el password utilizando el id de usuario
 $update_pass = "UPDATE docente SET password='$pass_secure', reset_password='0' WHERE id='$id_persona'";
 $ejec_update_pass = mysqli_query($conexion, $update_pass);
