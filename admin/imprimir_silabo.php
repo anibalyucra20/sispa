4d5a36980a5de173de2ba8d1d60a0c7512229708
anibalyucra20 <anibalyucrac@gmail.com>
2023-04-24 16:16:09 -0500

    Update imprimir_silabo.php

--- admin/imprimir_silabo.php
+++ admin/imprimir_silabo.php
@@ -216,7 +216,7 @@ function NbLines($w,$txt)
 	$pdf->Cell(70,6,utf8_decode('       Programa de Estudios'),0,0,'',0);
     $pdf->Cell(100,6,utf8_decode(": ".$r_b_pe['nombre']),0,1,'',0);
 	$pdf->Cell(70,6,utf8_decode('       Módulo'),0,0,'',0);
-    $pdf->Cell(0,6,utf8_decode(": ".$r_b_mod['descripcion']),0,0,'',0);
+    $pdf->Cell(100,6,utf8_decode(": ".$r_b_mod['descripcion']),1,1,'',0);
     $pdf->Cell(70,6,utf8_decode('       Unidad Didáctica'),0,0,'',0);
     $pdf->Cell(100,6,utf8_decode(": ".$r_b_ud['descripcion']),0,1,'',0);
     $pdf->Cell(70,6,utf8_decode('       Créditos'),0,0,'',0);
