0e8ba7f0e4de36c635e402aad20a421bb19aee88
anibalyucra20 <anibalyucrac@gmail.com>
2023-06-12 09:29:45 -0500

    Update reporte_consolidado_semestre.php

--- admin/reporte_consolidado_semestre.php
+++ admin/reporte_consolidado_semestre.php
@@ -228,25 +228,6 @@
                                 }
 
 
-                                if ($suma_calificacion != 0) {
-                                  $calificacion_final = round($suma_calificacion);
-                                } else {
-                                  $calificacion_final = "";
-                                }
-                                
-                                if ($r_b_det_mat_est['recuperacion'] != '') {
-                                  $calificacion_final = $r_b_det_mat_est['recuperacion'];
-                                }
-                                /*if ($calificacion_final > 12) {
-                                  echo '<th><center><font color="blue">'.$calificacion_final.'</font></center></th>';
-                                  //echo '<th><center><input type="number" class="nota_input" style="color:blue;" value="' . $calificacion_final . '" min="0" max="20" disabled></center></th>';
-                                } else {
-
-                                  echo '<th><center><font color="red">'.$calificacion_final.'</font></center></th>';
-                                  //echo 
-                                }*/
-
-
 
                               }else {
                                 //buscar los indicadores
