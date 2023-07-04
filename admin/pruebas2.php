8d340c8413cd2b5be4487b2cbea4624b9e5a372c
anibalyucra20 <anibalyucrac@gmail.com>
2023-07-04 14:48:13 -0500

    Update pruebas2.php

--- admin/pruebas2.php
+++ admin/pruebas2.php
@@ -2,6 +2,7 @@
 include 'include/verificar_sesion_docente_coordinador_secretaria.php';
 include '../include/conexion.php';
 include 'include/busquedas.php';
+include 'include/funciones.php';
 $id_prog = $_GET['id'];
 $b_prog = buscarProgramacionById($conexion, $id_prog);
 $cont_res = mysqli_num_rows($b_prog);
@@ -251,59 +252,10 @@ if (!($mostrar_archivo)) {
                                   <td><?php echo $r_b_est['apellidos_nombres']; ?></td>
                                   <?php
                                   //buscar las calificaciones
-                                  $b_calificaciones = buscarCalificacionByIdDetalleMatricula($conexion, $r_b_det_mat['id']);
-                                  $suma_calificacion = 0;
-                                  $cont_calif = 0;
-                                  while ($r_b_calificacion = mysqli_fetch_array($b_calificaciones)) {
-
-                                    $id_calificacion = $r_b_calificacion['id'];
-                                    //buscamos las evaluaciones
-                                    $suma_evaluacion = 0;
-
-                                    $b_evaluacion = buscarEvaluacionByIdCalificacion($conexion, $id_calificacion);
-                                    while ($r_b_evaluacion = mysqli_fetch_array($b_evaluacion)) {
-                                      $id_evaluacion = $r_b_evaluacion['id'];
-                                      //buscamos los criterios de evaluacion
-                                      $b_criterio_evaluacion = buscarCriterioEvaluacionByEvaluacion($conexion, $id_evaluacion);
-                                      $suma_criterios = 0;
-                                      $cont_crit = 0;
-                                      while ($r_b_criterio_evaluacion = mysqli_fetch_array($b_criterio_evaluacion)) {
-                                        if (is_numeric($r_b_criterio_evaluacion['calificacion'])) {
-                                          $suma_criterios += $r_b_criterio_evaluacion['calificacion'];
-                                          $cont_crit += 1;
-                                          //$suma_criterios += (($r_b_criterio_evaluacion['ponderado']/100)*$r_b_criterio_evaluacion['calificacion']);
-                                        }
-                                      }
-                                      if ($cont_crit > 0) {
-                                        $suma_criterios = round($suma_criterios / $cont_crit);
-                                      } else {
-                                        $suma_criterios = round($suma_criterios);
-                                      }
-
-                                      if (is_numeric($r_b_evaluacion['ponderado'])) {
-                                        $suma_evaluacion += ($r_b_evaluacion['ponderado'] / 100) * $suma_criterios;
-                                      }
-                                    }
-                                    $suma_calificacion += $suma_evaluacion;
-                                    if ($suma_evaluacion > 0) {
-                                      $cont_calif += 1;
-                                    }
-
-
-
-                                    if ($suma_evaluacion != 0) {
-                                      $calificacion = round($suma_evaluacion);
-                                    } else {
-                                      $calificacion = "";
-                                    }
-                                    if ($calificacion > 12) {
-                                      echo '<td><center><font color="blue">' . $calificacion . '</font></center></td>';
-                                      //echo '<td><center><input type="number" class="nota_input" style="color:blue;" value="' . $calificacion . '" min="0" max="20" disabled></center></td>';
-                                    } else {
-                                      echo '<td><center><font color="red">' . $calificacion . '</font></center></td>';
-                                      //echo '<td><center><input type="number" class="nota_input" style="color:red;" value="' . $calificacion . '" min="0" max="20" disabled></center></td>';
-                                    }
-                                  }
+                                  $id_det_matt = $r_b_det_mat['id'];
+                                  $suma_calificacion = calc_calificacion($conexion, $id_det_mat);
+                                  
+                                  
                                   ?>
 
 
