6fb44f9299f64420650a6d0b40f51e207363ec9a
anibalyucra20 <anibalyucrac@gmail.com>
2023-07-04 17:39:19 -0500

    update

--- admin/include/funciones.php
+++ admin/include/funciones.php
@@ -7,20 +7,33 @@
 //funcion para calcular promedio de los criterios de evaluacion
 function calc_criterios($conexion, $id_evaluacion)
 {
-    
+    $b_criterio_evaluacion = buscarCriterioEvaluacionByEvaluacion($conexion, $id_evaluacion);
+    $suma_criterios = 0;
+    $cont_crit = 0;
+    while ($r_b_criterio_evaluacion = mysqli_fetch_array($b_criterio_evaluacion)) {
+        if (is_numeric($r_b_criterio_evaluacion['calificacion'])) {
+            $suma_criterios += $r_b_criterio_evaluacion['calificacion'];
+            $cont_crit += 1;
+            //$suma_criterios += (($r_b_criterio_evaluacion['ponderado']/100)*$r_b_criterio_evaluacion['calificacion']);
+        }
+    }
+    if ($cont_crit > 0) {
+        $suma_criterios = round($suma_criterios / $cont_crit);
+    } else {
+        $suma_criterios = round($suma_criterios);
+    }
+    return $suma_criterios;
 }
 
 //funcion para calcular la la evaluacion(criterio de evaluacion) por ponderado
 function calc_evaluacion($conexion, $id_calificacion)
 {
-    
 }
 
 //funcion para calcular el promedio final
 
 function calc_calificacion($conexion, $id_det_mat)
 {
-    
 }
 
 /// FIN DE FUNCIONES DE CALIFICACIONES ------------------------------------------------------------
--- admin/pruebas2.php
+++ admin/pruebas2.php
@@ -265,21 +265,7 @@ if (!($mostrar_archivo)) {
                                     while ($r_b_evaluacion = mysqli_fetch_array($b_evaluacion)) {
                                       $id_evaluacion = $r_b_evaluacion['id'];
                                       //buscamos los criterios de evaluacion
-                                      $b_criterio_evaluacion = buscarCriterioEvaluacionByEvaluacion($conexion, $id_evaluacion);
-                                      $suma_criterios = 0;
-                                      $cont_crit = 0;
-                                      while ($r_b_criterio_evaluacion = mysqli_fetch_array($b_criterio_evaluacion)) {
-                                        if (is_numeric($r_b_criterio_evaluacion['calificacion'])) {
-                                          $suma_criterios += $r_b_criterio_evaluacion['calificacion'];
-                                          $cont_crit += 1;
-                                          //$suma_criterios += (($r_b_criterio_evaluacion['ponderado']/100)*$r_b_criterio_evaluacion['calificacion']);
-                                        }
-                                      }
-                                      if ($cont_crit > 0) {
-                                        $suma_criterios = round($suma_criterios / $cont_crit);
-                                      } else {
-                                        $suma_criterios = round($suma_criterios);
-                                      }
+                                      $suma_criterios = calc_criterios($conexion, $id_evaluacion);
 
                                       if (is_numeric($r_b_evaluacion['ponderado'])) {
                                         $suma_evaluacion += ($r_b_evaluacion['ponderado'] / 100) * $suma_criterios;
