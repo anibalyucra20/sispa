18bcfc87634a11712347dc31b2e42b95717eda0d
anibalyucra20 <anibalyucrac@gmail.com>
2023-07-04 20:26:59 -0500

    Update reporte_consolidado_semestre.php

--- admin/reporte_consolidado_semestre.php
+++ admin/reporte_consolidado_semestre.php
@@ -196,32 +196,7 @@
 
                                   $id_calificacion = $r_b_calificacion['id'];
                                   //buscamos las evaluaciones
-                                  $suma_evaluacion = 0;
-                                  $b_evaluacion = buscarEvaluacionByIdCalificacion($conexion, $id_calificacion);
-
-                                  while ($r_b_evaluacion = mysqli_fetch_array($b_evaluacion)) {
-                                    $id_evaluacion = $r_b_evaluacion['id'];
-                                    //buscamos los criterios de evaluacion
-                                    $b_criterio_evaluacion = buscarCriterioEvaluacionByEvaluacion($conexion, $id_evaluacion);
-                                    $suma_criterios = 0;
-                                    $cont_crit = 0;
-                                    while ($r_b_criterio_evaluacion = mysqli_fetch_array($b_criterio_evaluacion)) {
-                                      if (is_numeric($r_b_criterio_evaluacion['calificacion'])) {
-                                        $suma_criterios += $r_b_criterio_evaluacion['calificacion'];
-                                        $cont_crit += 1;
-                                        //$suma_criterios += (($r_b_criterio_evaluacion['ponderado']/100)*$r_b_criterio_evaluacion['calificacion']);
-                                      }
-                                    }
-                                    if ($cont_crit > 0) {
-                                      $suma_criterios = round($suma_criterios / $cont_crit);
-                                    } else {
-                                      $suma_criterios = round($suma_criterios);
-                                    }
-                                    if (is_numeric($r_b_evaluacion['ponderado'])) {
-                                      $suma_evaluacion += ($r_b_evaluacion['ponderado'] / 100) * $suma_criterios;
-                                    }
-                                    
-                                  }
+                                  $suma_evaluacion = calc_evaluacion($conexion, $id_calificacion);
 
                                   if ($suma_evaluacion != 0) {
                                     $suma_evaluacion = round($suma_evaluacion);
