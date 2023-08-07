dbcb0b429ea5fc205c448dc77091c35c07df2814
anibalyucra20 <anibalyucrac@gmail.com>
2023-08-07 12:14:50 -0500

    Update funciones.php

--- docente/include/funciones.php
+++ docente/include/funciones.php
@@ -43,7 +43,7 @@ function sesion_si_activa($conexion, $id_sesion, $token)
     $fecha_hora_fin = strtotime('+1 hour', strtotime($fecha_hora_fin_sesion));
     $fecha_hora_fin = date("Y-m-d h:i:s", $fecha_hora_fin);
 
-    if ((password_verify($r_b_sesion['token'], $token))&&($hora_actual<$fecha_hora_fin)) {
+    if ((password_verify($r_b_sesion['token'], $token))&&($hora_actual<=$fecha_hora_fin)) {
         actualizar_sesion($conexion, $id_sesion);
         return 1;
     }else {
